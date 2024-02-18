86068babe056abeb4c29a6be33834e7ea3546c9b
MISinuhaji <74509732+MISinuhaji@users.noreply.github.com>
2024-02-18 11:07:49 -0500

    Delete script.php

--- script.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Retrieve the data sent from JavaScript
-$myData = $_POST['myData'];
-
-$file = fopen('textData/markerLocations.txt', 'a');
-fwrite($file, $myData . "\n");
-fclose($file);
-?>
